22d47b34c3738ccddbd855fc38296415e216afe0
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-05-25 15:14:21 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import Sidebar from './components/Sidebar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      <Sidebar />
+
+      </div>
+    );
+  }
+}
+
+export default App;
